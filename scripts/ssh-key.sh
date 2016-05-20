ef3c27f98f1df2373e2833fdda4a798a39a79a4a
Henning Janßen <development@henning-janssen.net>
2016-05-20 14:45:28 +0200

    Add script for generating ssh-keys

---
+++ scripts/ssh-key.sh
@@ -0,0 +1,12 @@
+# Ask for the key-file
+read -p "File to store the key in: " kf
+
+# Generate key
+# The corresponding e-mail-address is passed as the first parameter
+ssh-keygen -t rsa -b 4096 -C "$1" -f $kf
+
+# start the ssh-agent in the background
+eval "$(ssh-agent -s)"
+
+# add the key
+ssh-add $kf
