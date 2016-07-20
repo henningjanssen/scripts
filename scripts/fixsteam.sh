1bf44f85e9f90f9a1baeae380cc67bd4be5e2223
Henning Janßen <development@henning-janssen.net>
2016-07-20 19:21:07 +0200

    fixsteam: disable more libs

--- scripts/fixsteam.sh
+++ scripts/fixsteam.sh
@@ -1,1 +1,4 @@
-mv ~/.local/share/Steam/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu/libgcc_s.so.1{,.disable}
+mv ~/.steam/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu/libgcc_s.so.1{,.disable}
+mv ~/.steam/ubuntu12_32/steam-runtime/amd64/lib/x86_64-linux-gnu/libgcc_s.so.1{,.disable}
+mv ~/.steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/libstdc++.so.6{,.disable}
+mv ~/.steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/libstd++.so.6{,.disable}
