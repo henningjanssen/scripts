91cc2a8031aee4df99927f9159fd25cec9af7ea1
Henning Janßen <henning.janssen@wwu.de>
2021-05-10 15:51:13 +0200

    script to change cpu governor

---
+++ scripts/cpu-governor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Arg: $1"
+
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+	echo "Available args: performance, powersave"
+	exit 0
+fi
+if [ "$1" == "performance" ]; then
+	echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+else
+	echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+fi
+
+echo "Status:"
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
