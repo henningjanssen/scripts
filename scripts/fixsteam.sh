255cac245c821d01ea7cab4fc7a95f82667cc744
Henning Janßen <henning.janssen@wwu.de>
2021-08-15 17:47:55 +0200

    rm fixsteam script

--- scripts/fixsteam.sh
+++
@@ -1,4 +0,0 @@
-mv ~/.steam/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu/libgcc_s.so.1{,.disable}
-mv ~/.steam/ubuntu12_32/steam-runtime/amd64/lib/x86_64-linux-gnu/libgcc_s.so.1{,.disable}
-mv ~/.steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/libstdc++.so.6{,.disable}
-mv ~/.steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/libstd++.so.6{,.disable}
